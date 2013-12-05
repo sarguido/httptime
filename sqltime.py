9a611ba4f3386038d338c2871ea244641f6f0f7e
Sarah Guido <sarguido@umich.edu>
2013-12-05 13:28:06 -0500

    Added more functions to the class and the general module.

--- sqltime.py
+++ sqltime.py
@@ -1,4 +1,4 @@
-'''A collection of date/time functions for sqlite3.
+'''A collection of date/time fun stuff that I've used mostly with sqlite3.
 Written out of frustration with the current datetime sqlite3 methods.'''
 
 import sqlite3
@@ -20,17 +20,23 @@ class DateSlashTimeColon(object):
 		time = re.search(r':([0-9]{2}:[0-9]{2}:[0-9]{2})', self.timestamp)
 		return date.group(0), time.group(1)
 
-class MathTime(object):
+	# Get separate date pieces
+	def date_pieces(self):
+		date, time = self.date_and_time()
+		day, month, year = date.split('/')
+		return day, month, year
 
-	def __init_(self, time):
-		self.time = time
+	def time_pieces(self):
+		date, time = self.date_and_time()
+		hour, minute, second = time.split(':')
+		return hour, minute, second
 
-	# Converts time to seconds to calculation
-	def time_to_seconds(self):
-		h, m, s = [int(i) for i in self.time.split(':')]
-		print (3600 * h) + (60 * m) + s
+# Converts time to seconds to calculation
+def time_to_seconds(time):
+	h, m, s = [int(i) for i in time.split(':')]
+	print (3600 * h) + (60 * m) + s
 
-	# Calculates amount of time spend on item and converts seconds back to time format
-	'''def calculate_time(time1, time2):
-		sec = time_to_seconds(time2) - time_to_seconds(time1)
-		return datetime.timedelta(seconds = sec)'''
+# Calculates time difference
+def calculate_time_difference(time1, time2):
+	sec = time_to_seconds(time2) - time_to_seconds(time1)
+	return datetime.timedelta(seconds = sec)
