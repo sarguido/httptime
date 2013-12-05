03cf6787f870a37365526daa9ee97227e0d48de1
Sarah Guido <sarguido@umich.edu>
2013-12-05 15:59:35 -0500

    Added more functions and a TODO.

--- sqltime.py
+++ sqltime.py
@@ -1,5 +1,9 @@
-'''A collection of date/time fun stuff that I've used mostly with sqlite3.
-Written out of frustration with the current datetime sqlite3 methods.'''
+'''TODO:
+-Flesh out object(s)
+-Add date functions
+-Refactor for better
+-Documentation
+-Examples'''
 
 import sqlite3
 import re
@@ -20,23 +24,38 @@ class DateSlashTimeColon(object):
 		time = re.search(r':([0-9]{2}:[0-9]{2}:[0-9]{2})', self.timestamp)
 		return date.group(0), time.group(1)
 
-	# Get separate date pieces
+	# Get separate date pieces - returns as str
 	def date_pieces(self):
 		date, time = self.date_and_time()
 		day, month, year = date.split('/')
 		return day, month, year
 
+	# Get separate time pieces - returns as str
 	def time_pieces(self):
 		date, time = self.date_and_time()
 		hour, minute, second = time.split(':')
 		return hour, minute, second
 
-# Converts time to seconds to calculation
+# Time functions
+
+# Converts time from timestamp to seconds
 def time_to_seconds(time):
 	h, m, s = [int(i) for i in time.split(':')]
-	print (3600 * h) + (60 * m) + s
+	return (3600 * h) + (60 * m) + s
+
+# Converts seconds to time
+def seconds_to_time(seconds):
+	return datetime.timedelta(seconds = sec)
 
 # Calculates time difference
 def calculate_time_difference(time1, time2):
 	sec = time_to_seconds(time2) - time_to_seconds(time1)
 	return datetime.timedelta(seconds = sec)
+
+# Calculates time addition
+def calculate_time_addition(time1, time2):
+	sec = time_to_seconds(time2) + time_to_seconds(time1)
+	return datetime.timedelta(seconds = sec)
+
+# Date functions
+
